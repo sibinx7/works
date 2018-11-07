30fc2d743d6ff7e43648e7e51d23fa39a3dd2b12
Sibin Xavier <sibinx7@gmail.com>
2018-11-07 22:25:14 +0530

    Development and Production url origin

--- src/api/env.js
+++ src/api/env.js
@@ -1,8 +1,15 @@
-debugger 
-let API_URL = API_URL || window.API_URL;
+let hostname = window.location.hostname
+let API_URL = "http://7chip.local";
+
+if(hostname === "works.7chip.com"){
+  API_URL = "http://7chip.com";
+}
+
+console.log(API_URL);
+console.log("HOST NAME")
 const settings = {
   API_URL,
-  PORTFOLIO_URL: `${API_URL}/world/websites`
+  PORTFOLIO_URL: `${API_URL}/api/world/websites`
 };
 
 export default settings;
